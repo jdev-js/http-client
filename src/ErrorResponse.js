2b14c3e0f3febd782f4e5d2eecf080b48ed9e7e6
jdevjs <jesusalcalarojas@gmail.com>
2019-12-07 16:39:58 -0400

    remove error for name and for strucure project

--- src/ErrorResponse.js
+++
@@ -1,8 +0,0 @@
-export default class ErrorResponse extends Error {
-  constructor() {
-    super()
-    this.name = 'ErrorResponse'
-    this.message = 'Error at revice the response'
-    this.stack = ''
-  }
-}
